9d70e58fb90dae29900411ea94e98bbcb6695fb0
solarphaces <lgdanielito@gmail.com>
2017-04-11 20:58:07 -0400

    hell

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(){
+	// console.log("Let's add");
+	// var math = ;
+	console.log(7 + 7);
+}
+
+function subtract(){
+	// console.log("Let's subtract")
+	// var math = ;
+	console.log(7 -7);
+}
+
+function multiply(){
+	// console.log("Let's multiply")
+	var math = 7 * 7;
+	console.log(math);
+}
+
+function divide(){
+	// console.log("Let's divide")
+	var math = 7 / 7;
+	console.log(math);
+}
+
